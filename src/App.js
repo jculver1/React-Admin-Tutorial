d6d09d3ecc810730cb8c221c3679878be364eedd
jculver1 <jculver1900@gmail.com>
2019-05-30 09:13:45 -0600

    added custom user field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import Users from './components/Users'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+     <Resource name="users" list={Users} />
   </Admin>
 )
 
